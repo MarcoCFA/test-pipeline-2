db89604f9a68a5b916db80e646009a838eee34e9
Marco Naylon <marconaylon@gmail.com>
2021-11-11 15:06:24 -0700

    updated environment to us-east-1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import aws_cdk as cdk
 
 from test_pipeline_2.test_pipeline_2_stack import TestPipeline2Stack
 
-APP_ENV = cdk.Environment(account="225342792054", region="us-east-2")
+APP_ENV = cdk.Environment(account="225342792054", region="us-east-1")
 
 app = cdk.App()
 TestPipeline2Stack(app, "TestPipeline2Stack", env=APP_ENV)
