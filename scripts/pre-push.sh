979e88b1e0d5c7fd38b6e5de8eeca59f3fcf548d
Marco Naylon <marconaylon@gmail.com>
2021-11-15 15:18:49 -0700

    chore: commit save point

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+echo "Running pre-push.sh"
+
 echo_failed_tests () {
   echo "Tests must pass before push."
   exit 1
@@ -12,3 +15,5 @@ if [ $status -ne 0 ]; then
   echo_failed_tests
 fi
 
+
+echo "Exiting pre-push.sh"
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo "Running run_tests.sh"
+
+echo "Exiting run_tests.sh"
