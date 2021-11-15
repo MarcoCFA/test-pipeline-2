ce801b5031bd1ec861d059de3816e40cc79f5bc8
Marco Naylon <marconaylon@gmail.com>
2021-11-15 10:23:32 -0700

    test black pre-commit hook

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "Running pre-commit.sh"
+echo "Starting pre-commit.sh"
+
+echo_failed_tests () {
+  echo "Tests must pass before commit."
+  exit 1
+}
+
+
+echo "Running black formatting test"
+# 1 - Go to the git root directory using pushd to navigate the directory stack
+# 2 - Run black formatting on root directory
+# 3 - Remove the root directory from the directory stack
+
+pushd "$(git rev-parse --show-toplevel)" || return 1
+black .
+popd || return 1
+
