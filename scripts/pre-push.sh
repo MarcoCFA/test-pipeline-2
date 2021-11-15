a0eeb7743e8ebeb94a157910c4d7feb6df4f5eef
Marco Naylon <marconaylon@gmail.com>
2021-11-15 15:14:23 -0700

    test pre-push cdk synth

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-echo "Call pre-push.sh"
+echo_failed_tests () {
+  echo "Tests must pass before push."
+  exit 1
+}
+
+echo "Running cdk synth"
+cdk synth
+status=$?
+if [ $status -ne 0 ]; then
+  echo_failed_tests
+fi
 
